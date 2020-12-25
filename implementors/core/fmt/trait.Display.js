2449f4d5548b6c3be03f7b27b48376e5cb9a34a8
Arjun Raghavan <arjun.raghavan.2k@gmail.com>
2020-12-25 18:06:49 -0500

    Update documentation

--- implementors/core/fmt/trait.Display.js
+++ implementors/core/fmt/trait.Display.js
@@ -1,6 +1,3 @@
 (function() {var implementors = {};
-implementors["getrandom"] = [{"text":"impl Display for Error","synthetic":false,"types":[]}];
 implementors["ml_rust_cuda"] = [{"text":"impl Display for Matrix","synthetic":false,"types":[]},{"text":"impl Display for Vector","synthetic":false,"types":[]}];
-implementors["rand"] = [{"text":"impl Display for BernoulliError","synthetic":false,"types":[]},{"text":"impl Display for WeightedError","synthetic":false,"types":[]},{"text":"impl Display for ReadError","synthetic":false,"types":[]}];
-implementors["rand_core"] = [{"text":"impl Display for Error","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
